d2fb9e6255fa361c98ccb86930ccd9f315f81b91
Dowon Wang <dowonwang01@gmail.com>
2026-03-05 17:18:05 +0900

    chore(server/api): docker mysql 설정
    - docker compose 파일 작성
    - 환경변수 추가 및 적용
    - dotenv 패키지 추가

--- apps/server/api/src/index.ts
+++ apps/server/api/src/index.ts
@@ -1,7 +1,10 @@
 import { Elysia } from "elysia";
+import "dotenv/config";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .listen(process.env.APP_PORT || 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
