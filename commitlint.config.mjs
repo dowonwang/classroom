058eed967964a8ad84bb66a87468d8c476bb8b45
Dowon Wang <dowonwang01@gmail.com>
2026-03-12 20:06:06 +0900

    chore: commitlint scope 설명 추가

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -105,7 +105,7 @@ export default {
         description:
           '이 변경 사항이 영향을 미치는 범위는 어디인가요? (예: 컴포넌트명 또는 파일명)',
         enum: {
-          api: { description: 'apps/server/api' },
+          'server/api': { description: 'apps/server/api' },
         },
       },
       subject: {
