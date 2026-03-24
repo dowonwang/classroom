e89581001366c36d57cead4c97449ac80f5e2842
Dowon Wang <dowonwang01@gmail.com>
2026-03-24 19:01:59 +0900

    feat(server/api): prisma 예외 처리 추가

--- apps/server/api/src/shared/http/plugin/error.plugin.ts
+++ apps/server/api/src/shared/http/plugin/error.plugin.ts
@@ -5,7 +5,9 @@ import { LOG_MESSAGE } from '../../logger/constant/log-message';
 import { logger } from '../../logger/logger';
 import { ApiResponseBuilder } from '../../responses/api-response-builder';
 import { getRouteLogScope } from '../constants/route-log-scope';
+import { PrismaErrorMapper } from '../mapper/prisma-error-mapper';
 import { VaildationErrorMapper } from '../mapper/vaildation-error.mapper';
+import { Prisma } from '@packages/api-db';
 import { randomUUIDv7 } from 'bun';
 import Elysia, { ValidationError } from 'elysia';
 
@@ -69,6 +71,51 @@ export const errorPlugin = new Elysia().onError(
 
       return ApiResponseBuilder.error({
         message: error.userMessage,
+        requestId: uuid,
+      });
+    }
+
+    const prismaError = PrismaErrorMapper(error);
+
+    if (prismaError) {
+      set.status = prismaError.status;
+
+      if (
+        error instanceof Prisma.PrismaClientRustPanicError ||
+        error instanceof Prisma.PrismaClientInitializationError
+      ) {
+        logger.fatal(
+          {
+            event: prismaError.event,
+            requestId: uuid,
+            method: request.method,
+            path,
+            searchParams,
+            status: prismaError.status,
+            details: prismaError.details,
+            scope: prismaError.scope || 'APP',
+          },
+          prismaError.message,
+        );
+      } else {
+        logger.error(
+          {
+            event: prismaError.event,
+            requestId: uuid,
+            method: request.method,
+            path,
+            searchParams,
+            status: prismaError.status,
+            details: prismaError.details,
+            scope: prismaError.scope || 'APP',
+            err: prismaError.cause,
+          },
+          prismaError.message,
+        );
+      }
+
+      return ApiResponseBuilder.error({
+        message: prismaError.userMessage,
         requestId: uuid,
       });
     }
